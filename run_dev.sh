833d18d4f756319883461e257406c3f2019d2c8c
bhanreddy <reddybhanu283@gmail.com>
2025-11-19 12:29:36 +0530

    first commit

--- run_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# helper for unix/mac - run backend and serve frontend
-cd backend
-uvicorn main:app --reload &
-SERVER_PID=$!
-cd ../frontend
-python -m http.server 3000 &
-wait $SERVER_PID
